27cb444bda37e5666ee57767d13ad46e727e9e9a
ARElton <ameliareneeelton@gmail.com>
2021-01-18 16:58:42 -0500

    update

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer( { sushis, eatSushi, onClickMore}) {
+
+  console.log(sushis)
+
+  const sushiDetails = sushis.map((sushi) => (
+    <Sushi key={sushi.id} sushi={sushi} eatSushi={eatSushi} />
+  ))
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiDetails}
+      <MoreButton onClickMore={onClickMore} />
     </div>
   );
 }
